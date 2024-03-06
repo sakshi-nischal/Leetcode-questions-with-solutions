8661c29029587f77de49683a3c211e8997a3a378
Nelson Joseph <63466039+nelson123-lab@users.noreply.github.com>
2024-03-06 02:46:57 -0600

    Added solution Reverse Linked List.py

--- Leetcode 75/Linked List/206. Reverse Linked List.py
+++ Leetcode 75/Linked List/206. Reverse Linked List.py
@@ -1,1 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr = None, head
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        return prev
 
+"""
+- Here the algorithm works by first storing the next values in a temp variable called nxt and then assigning the values of curr.next = prev ( initailly None ).
+- It then changes the prev value to the current value and finally current value to the next value stored in the nxt variable.
+- We finally return the head of the reversed linkedList that is the prev.
+Time COmplexity O(n)
+Space COmplexity O(1)
+"""
+
+# Reference:
+# https://youtu.be/G0_I-ZF0S38
