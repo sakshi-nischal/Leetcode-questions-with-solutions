5e7f89506bf74619addfe30c07b2cd922d417135
Sakshi Nischal <57134054+sakshi-nischal@users.noreply.github.com>
2024-03-17 00:51:03 -0500

    Update 1211. Queries Quality and Percentage.sql

--- SQL 50/Basic Aggregate Function/1211. Queries Quality and Percentage.sql
+++ SQL 50/Basic Aggregate Function/1211. Queries Quality and Percentage.sql
@@ -4,3 +4,11 @@ round(AVG(CASE WHEN rating<3 THEN 1 ELSE 0 END)*100,2) as poor_query_percentage
 FROM Queries
 WHERE query_name is not null
 GROUP BY query_name
+
+
+-- Approach 2
+select q.query_name, round(avg(q.rating/q.position),2) quality, ifnull(round((p.rating_count/count(q.rating))*100, 2),0) poor_query_percentage
+from queries as q left join (select query_name, count(rating) as rating_count from queries where rating < 3 group by query_name) p
+on q.query_name = p.query_name
+where q.query_name is not null
+group by q.query_name
